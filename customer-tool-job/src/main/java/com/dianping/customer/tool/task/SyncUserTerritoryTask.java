7373edcb6b195c4b0093d75b1d71eac73e516865
Arnold Wang <zhenwei.wang@dianping.com>
2014-12-31 17:03:20 +0800

    change dataloader jar

--- customer-tool-job/src/main/java/com/dianping/customer/tool/task/SyncUserTerritoryTask.java
+++ customer-tool-job/src/main/java/com/dianping/customer/tool/task/SyncUserTerritoryTask.java
@@ -44,7 +44,7 @@ public class SyncUserTerritoryTask {
 		}
 
 		try {
-			System.setProperty("appdata.dir", "/data");
+//			System.setProperty("appdata.dir", "/data");
 			controller = Controller.getInstance("ui", false);
 		} catch (ControllerInitializationException e) {
 			logger.warn("controller init failed! Task will not run!", e);
